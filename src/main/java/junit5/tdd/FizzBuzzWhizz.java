9e05a90df86e16167acb1cabd5b431d0f9270c28
userName <gongl3730@gmail.com>
2020-07-23 17:50:14 +0800

    [FizzBuzzWhizz][Bruno] feature: implement test method given 21 when judge FizzWhizz then result is FizzWhizz

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -3,6 +3,9 @@ package junit5.tdd;
 public class FizzBuzzWhizz {
 
     public String playFizzBuzzWhizz(int number) {
+        if (number % 3 == 0 && number % 7 == 0) {
+            return "FizzWhizz";
+        }
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
         }
