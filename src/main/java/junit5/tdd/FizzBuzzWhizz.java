335ef4197e45f41a5160e85759b1b236b880e581
userName <gongl3730@gmail.com>
2020-07-23 17:09:01 +0800

    [FizzBuzzWhizz][Bruno] feature: implement test method given 1 when judge 1 then result is nu1

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -2,7 +2,7 @@ package junit5.tdd;
 
 public class FizzBuzzWhizz {
 
-    public String playFizzBuzzWhizz() {
-
+    public String playFizzBuzzWhizz(int number) {
+        return String.valueOf(number);
     }
 }
